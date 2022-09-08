5b58a6704d2656f3cadc0299cea057110b3a1fc9
GUchechi <godswilluchendu7@gmail.com>
2022-09-08 18:57:50 +0100

    react click events

--- src/Content.jsx
+++ src/Content.jsx
@@ -6,11 +6,25 @@ const Content = () => {
         const int = Math.floor(Math.random() * names.length);
         return names[int];
       }
+
+    const handleClick = () => {
+      console.log("You clicked it!")
+    }
+    const handleClick2 = (name) => {
+      console.log(`${name} was clicked also!`)
+    }
+
+    const handleClick3 = (e) => {
+      console.log(e.target.innerText)
+    }
   return (
     <main>
         <p>
             Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click it</button>
+        <button onClick={() =>handleClick2("Dave")}>Click it</button>
+        <button onClick={ (e) => handleClick3(e)}>Click it</button>
     </main>
   )
 }
