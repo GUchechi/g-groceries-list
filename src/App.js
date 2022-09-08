0381af1e4a3b51475229ac799b75fd11eb11ce15
GUchechi <godswilluchendu7@gmail.com>
2022-09-08 13:48:01 +0100

    created a header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['Bob', 'John', 'Dave']
+    const int = Math.floor(Math.random() * names.length);
+    return names[int];
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
     </div>
   );
 }
