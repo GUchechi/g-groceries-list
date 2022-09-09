ed2619ac7d063ebe41b085034c795957cb1dd02a
GUchechi <godswilluchendu7@gmail.com>
2022-09-09 11:56:02 +0100

    added map loop

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,32 +1,37 @@
 import React, { useState } from 'react'
 
 const Content = () => {
-  const [name, setName] = useState("Dave")
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "One half pound bag of Cocoa Covered Almonds Unsalted"
+  },
+  {
+      id: 2,
+      checked: false,
+      item: "Item 2"
+  },
+  {
+      id: 3,
+      checked: true,
+      item: "Item 3"
+  }
+]);
 
-    const handleNameChange = () => {
-        const names = ['Bob', 'John', 'Dave']
-        const int = Math.floor(Math.random() * names.length);
-        setName(names[int]);
-      }
-
-    const handleClick = () => {
-      setCount(count + 1)
-      console.log(count)
-    }
-    const handleClick2 = () => {
-      console.log(count)
-    }
-
-  
   return (
     <main>
-        <p>
-            Hello {name}!
-        </p>
-        <button onClick={handleNameChange}>Change name</button>
-        <button onClick={() =>handleClick()}>Click it</button>
-        <button onClick={ () => handleClick2()}>Click it</button>
+        <ul>
+          {items.map((item, index) => (
+            <li key={index} className="item">
+              <input type="checkbox" 
+                checked={item.checked}
+              />
+              <label>{item.item}</label>
+              <button>Delete</button>
+            </li>
+          ))}
+        </ul>
     </main>
   )
 }
