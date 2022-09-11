ad0040d12c0dc79367bf434a79b4ba2aecd8d39d
GUchechi <godswilluchendu7@gmail.com>
2022-09-11 18:21:51 +0100

    added controll input to add items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 
 import { useState } from 'react';
+import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
 import Header from './Header';
@@ -24,21 +25,46 @@ function App() {
     }
 ]);
 
+const [newItem, setNewItem] = useState("");
+
+const setAndSaveItems = (newItems) => {
+  setItems(newItems);
+  localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+}
+
+const addItem = (item) => {
+  const id = items.length ? items[items.length - 1].id + 1 : 1;
+  const myNewItem = { id, checked: false, item };
+  const listItems = [...items, myNewItem];
+  setAndSaveItems (listItems);
+}
+
 const handleCheck = (id) => {
   const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item);
-  setItems(listItems);
-  localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  setAndSaveItems (listItems);
 }
 
 const handleDelete = (id) => {
   const listItems = items.filter((item) => item.id !== id);
-  setItems(listItems);
-  localStorage.setItem('shoppinglist', JSON.stringify(listItems));
+  setAndSaveItems (listItems);
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if(!newItem) return;
+  // AddItem
+  addItem(newItem);
+  setNewItem('');
 }
 
   return (
     <div className="App">
      <Header title="Groceries List" />
+     <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
      <Content
       items={items}
       handleCheck={handleCheck}
