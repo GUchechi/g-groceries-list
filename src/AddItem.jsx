d79c5981cd16f883617d6400c9e3914eef72dbd3
GUchechi <godswilluchendu7@gmail.com>
2022-09-11 17:41:38 +0100

    added new component AddItem

--- src/AddItem.jsx
+++ src/AddItem.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { FaPlus} from 'react-icons/fa';
 
 const AddItem = () => {
   return (
-    <div>
-      
-    </div>
+    <form className="addForm">
+      <label htmlFor='additem'>Add Item</label>
+      <input
+        autoFocus
+        id='additem'
+        type='text'
+        placeholder='Add Item'
+        required
+    />
+        <button type="submit" aria-label='Add Item'>
+            <FaPlus />
+        </button>
+    </form>
   )
 }
 
