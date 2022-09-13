330db100d8d9b4f5e4653f701e0a2ee3c5924181
GUchechi <godswilluchendu7@gmail.com>
2022-09-13 11:32:47 +0100

    added fetch api data

--- src/App.js
+++ src/App.js
@@ -7,14 +7,31 @@ import Header from './Header';
 import SearchItem from './SearchItem';
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+const API_URL = 'http://localhost:3500/items';
+
+  const [items, setItems] = useState([]);
 
 const [newItem, setNewItem] = useState("");
 const [search, setSearch] = useState("");
+const [fetchError, setFetchError] = useState(null);
 
 useEffect(() => {
-  localStorage.setItem('shoppinglist', JSON.stringify(items))
-}, [items])
+  
+  const fetchItems = async () => {
+    try{
+      const response = await fetch(API_URL);
+      if (!response.ok) throw Error ('Did not receive expected data')
+      const listItems = await response.json();
+      console.log(listItems);
+      setItems(listItems);
+      setFetchError(null);
+    } catch(err) {
+      setFetchError(err.message);
+    }
+  }
+
+  (async () => await fetchItems())();
+}, [])
 
 const setAndSaveItems = (newItems) => {
   setItems(newItems);
@@ -58,11 +75,14 @@ const handleSubmit = (e) => {
         search={search}
         setSearch={setSearch}
      />
-     <Content
-      items={items.filter( item => (item.item).toLowerCase().includes(search.toLowerCase()))}
-      handleCheck={handleCheck}
-      handleDelete={handleDelete}
-      />
+     <main>
+        {fetchError && <p style={{color: 'red'}}>{`Error: ${fetchError}`}</p>}
+        {!fetchError && <Content
+        items={items.filter( item => (item.item).toLowerCase().includes(search.toLowerCase()))}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+        />}
+      </main>
      <Footer
       length={items.length}
       />
